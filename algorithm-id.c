6d72cfe795af64658f3ab4946d464e7eda0c4456
ClickCyber <66947475+ClickCyber@users.noreply.github.com>
2022-02-02 17:15:55 +0200

    Update algorithm-id.c

--- algorithm-id.c
+++ algorithm-id.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-
-bool id_is_valid(char * NumberID);
-
-bool id_is_valid(char * NumberID){
+#define AlgoLuhn 10
+#define MaxSize 9
+const bool IdIsValid(char * NumberID);
+bool IdIsValid(char * NumberID){
 	int Isummary = 0;
 	int IsIdCalc = 0;
-
-	for(int loop=0; loop < strlen(NumberID); loop++){
+	int ReturnInteger = * int(int Number){ return Number - '0';};
+	for(int i=0; i < strlen(NumberID); i++){
 		Isummary++;
-		int INumOneTime = NumberID[loop] - '0';
-		int dataLoop = INumOneTime * Isummary;
-		int loop_array[2] = {dataLoop, 0};
-		if (dataLoop > 9){
-			char data[10];
-			sprintf(data, "%d", loop_array[0]);
-			for(int SecondLoop = 0; SecondLoop < strlen(data); SecondLoop++){
-				int CalcNum = data[SecondLoop] - '0';
-				loop_array[1] += CalcNum;
-			}
-		IsIdCalc += loop_array[1];
+		char data[10];
+		int INumOneTime = ReturnInteger(NumberID[i]);
+		int DuobleLoop = INumOneTime * Isummary;
+		if (DuobleLoop > MaxSize){
+		sprintf(data, "%d", DuobleLoop);
+		for(int SecondLoop = 0; SecondLoop < strlen(data); SecondLoop++){
+			IsIdCalc += ReturnInteger(data[SecondLoop]);
+		}
 		}else
 			IsIdCalc += INumOneTime * Isummary;
-		if (Isummary == 2)
-			Isummary = 0;
+		(Isummary == 2) ? Isummary = 0 : Isummary;
 	}
-
-	if(IsIdCalc % 10 == 0)
-		return true;
-	else
-		return false;
+	return IsIdCalc % AlgoLuhn == 0;
 }
 
 int main(int argc, char * argv []){
-   	char SNumberID[9];
+   	char SNumberID[MaxSize];
    	strncpy(SNumberID, argv[1], sizeof(SNumberID));
-   	if(id_is_valid(SNumberID) == true 
-   		&& strlen(SNumberID) == 9)
+   	if(IdIsValid(SNumberID) == true 
+   		&& strlen(SNumberID) == MaxSize)
    		printf("Number ID %s is valid !", SNumberID);
    	else
    		printf("Number ID %s is invalid !", SNumberID);
